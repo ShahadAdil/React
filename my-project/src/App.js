c9e2cb01f4f24b1a4967b57fbd91d67a601ebd19
ShahadAdil <71069861+ShahadAdil@users.noreply.github.com>
2023-04-01 10:49:43 +0200

    update

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
-import NavBar from './components/NavBar.js'
+import NavBar from './components/NavBar';
+import HeroImage from './components/HeroImage';
+import Logo from './components/Logo';
+import Products from './components/products';
 
 function App() {
   return (
     <div className="App">
+        <Logo />
         <NavBar />
+        <HeroImage />
+        <Products />
 
     </div>
   );
