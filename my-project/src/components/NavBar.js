9415399d7d19d079be58c56f3a321501648f5329
ShahadAdil <71069861+ShahadAdil@users.noreply.github.com>
2023-04-05 12:03:06 +0200

    Edited NavBar

--- my-project/src/components/NavBar.js
+++ my-project/src/components/NavBar.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Link,Route,Routes} from 'react-router-dom';
-import BirthdayCakeBox from './BirthdayCakeBox';
-import GladPask from './GladPask';
+import Menu from './Menu';
+import GladPask from './Product';
 import Home from './Home'
 
 function NavBar () {
@@ -12,15 +12,15 @@ function NavBar () {
     <Link to="/home" className="nav-link active text-white">Home</Link>
   </li>
   <li className="nav-item">
-    <Link to="/birthdaycakebox" className="nav-link active text-white">BIRTHDAY CAKE BOX</Link>
+    <Link to="/menu" className="nav-link active text-white">MENU</Link>
   </li>
-  <li className="justify-content-end">
+  {/* <li className="justify-content-end">
     <Link to="/gladpask" className="nav-link text-white">GLAD PÅSK</Link>
-  </li>
+  </li> */}
 </ul>
  <Routes>
- <Route path="/birthdaycakebox" element={<BirthdayCakeBox />}> </Route>
- <Route path="/gladpask" element={<GladPask />}></Route>
+ <Route path="/menu" element={<Menu />}> </Route>
+ {/* <Route path="/gladpask" element={<GladPask />}></Route> */}
  <Route path="/home" element={<Home />}></Route>
 </Routes>
 </>
