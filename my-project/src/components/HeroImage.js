4695c1948279efdd5519b25aa53045728f356ac8
ShahadAdil <71069861+ShahadAdil@users.noreply.github.com>
2023-04-14 17:03:44 +0200

    update

--- my-project/src/components/HeroImage.js
+++ my-project/src/components/HeroImage.js
@@ -1,6 +1,19 @@
 import React from 'react'
+import  {Link}  from 'react-router-dom';
 
 const HeroImage = () => {
+
+// const goTO = Link();
+
+//     function goToMenu(){
+// goTO('/menu')
+//     }
+
+    function goToMenu(){
+// goTO('/menu')
+alert('Thank you for choosing us...')
+    }
+
   return (
     <div className='hero-image container'>
         <img src="images/HeroImg.PNG" alt="chocolate"></img>
@@ -8,7 +21,11 @@ const HeroImage = () => {
         <h1>BRUNCH</h1>
         <h3>CHECK OUT OUR MENU</h3>
         <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
-        <button type='submit'>MENU</button>
+        {/* <button  type='submit'>MENU</button> */}
+        {/* <button onClick={goToMenu} type='submit'>MENU</button> */}
+        <Link to="/menu">
+                    <button onClick={goToMenu} type='submit'>MENU</button>
+                </Link>
     </div>
     </div>
   )
